18f7f9b6e36c68af21654972e2066de960e524c5
aleksander.ujek <aleksander.ujek@gmail.com>
2025-10-14 21:01:14 +0200

    feat: integrate Supabase client and types, update package dependencies

---
+++ src/middleware/index.ts
@@ -0,0 +1,10 @@
+import type { MiddlewareHandler } from 'astro'
+
+import { supabaseClient } from '../db/supabase.client'
+
+export const onRequest: MiddlewareHandler = async (context, next) => {
+  context.locals.supabase = supabaseClient
+  return next()
+}
+
+
